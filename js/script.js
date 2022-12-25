d9528684dd6c10201415cd4cd825a7a13f6c77eb
Abdurrahman Al Hanif <abdurrahman.alhanif@gmail.com>
2022-12-25 03:45:25 +0000

    add css & js file

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* TEAM 2 UAS WDI
+ * ==============
+ * - Afif Whelly Artissandi (211210058)
+ * - Yohanes Crusc Yunianto Tinitih (211210061)
+ * - Wuri Handayani (211220059)
+ * - Arif Alifudin (211210064)
+ * - Abdurrahman Hanifatullah S. P. (211210075)
+ * 
+ * Copyright 2022 - License: CC BY 4.0
+ */
